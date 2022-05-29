d1220f79985d222c1c078231c2816156e0b7248c
Lakshmi <lakshmipbps@gmail.com>
2022-05-29 21:12:28 +0530

    first branch 'iss1' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,9 +1,17 @@
 package com.cts;
 
+import com.cts.service.SomeService;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome the session");
 		System.out.println("Hi, hope you are fine");
-	}
+		
+		SomeService ss=new SomeService();
+		System.out.println(ss.findLength("program"));
+		
+		}
+	
+	
 
 }
