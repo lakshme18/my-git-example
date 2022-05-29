b6eb052c3919536141d9607f5b7a72ec0ada82eb
Lakshmi <lakshmipbps@gmail.com>
2022-05-29 20:26:24 +0530

    initial commit

---
+++ src/com/cts/Main.java
@@ -0,0 +1,9 @@
+package com.cts;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome the session");
+		System.out.println("Hi, hope you are fine");
+	}
+
+}
